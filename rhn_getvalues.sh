e9c6ef197a15ca863a057ad564cf403667b84d37
Pablo <Pablo.Iranzo@gmail.com>
2012-02-23 12:43:00 +0100

    Added script for querying custom_infos for a profile

---
+++ rhn_getvalues.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+#
+# Script for getting custom info values for a system
+#
+
+. $RHNUTILS_PATH/rhn_common.sh
+
+python $RHNUTILS_PATH/rhn_getvalues.py -u $RHN_USER -p $RHN_PASS -s https://$RHN_SERVER/rpc/api
