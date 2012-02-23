4eafe360f0de6e0a012b1a3c5068bceada4ce2bf
Pablo <Pablo.Iranzo@gmail.com>
2012-02-23 12:46:21 +0100

    Added script for querying group memberships

---
+++ rhn_getgroup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+#
+# Description: Script for obtaining groupmembership for this host
+#
+
+. $RHNUTILS_PATH/rhn_common.sh
+
+SERVER=`cat /etc/sysconfig/rhn/up2date|grep serverURL=|cut -d "/" -f 3`
+python $RHNUTILS_PATH/rhn_getgroup.py -u $RHN_USER -p $RHN_PASS -s https://$RHN_SERVER/rpc/api
